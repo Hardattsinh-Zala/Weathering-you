673f3b9769b5a9c0b93008e6baf91aaed96039e7
Hardattsinh Zala <zalahardatt@gmail.com>
2025-04-18 14:39:45 +0530

    UI changes

--- src/components/FormPropsTextFields.jsx
+++ src/components/FormPropsTextFields.jsx
@@ -2,15 +2,26 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import "../index.css";
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+
+})
 
 export default function FormPropsTextFields({getValue, enter}) {
   return (
     <Box
+      className='sm:bg-black rounded-xl'
       component="form"
       sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       noValidate
       autoComplete="off">
       <TextField
+        sx={{
+          input: {color: 'white'},
+          label: {color: 'white'}
+        }}
         id="standard-search"
         label="Search field"
         type="search"
